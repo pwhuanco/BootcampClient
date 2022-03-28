8be567beab4a0f4aaf218c801b5b6fe79b811099
pwhuanco <pedrohuanco@gmail.com>
2022-03-28 11:47:07 -0500

    para redesplegar

--- bankclient/src/main/java/com/bootcamp/bankclient/BankClientApplication.java
+++ bankclient/src/main/java/com/bootcamp/bankclient/BankClientApplication.java
@@ -1,5 +1,8 @@
 package com.bootcamp.bankclient;
 
+import com.bootcamp.bankclient.controller.ClientController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,8 +10,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class BankClientApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BankClientApplication.class);
 
     public static void main(String[] args) {
+        LOGGER.debug("--Iniciando BankClientApplication--");
         SpringApplication.run(BankClientApplication.class, args);
     }
 
